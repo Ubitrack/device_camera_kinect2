da6767855d95495b8083fa4e808309d45b99039e
Ulrich Eck <ueck@net-labs.de>
2019-08-27 08:45:11 +0200

    add system requirements for linux/ubuntu

--- conanfile.py
+++ conanfile.py
@@ -27,6 +27,42 @@ class UbitrackCoreConan(ConanFile):
     # all sources are deployed with the package
     exports_sources = "cmake/*", "doc/*", "src/*", "CMakeLists.txt"
 
+    def system_requirements(self):
+        if os_info.is_linux:
+            if os_info.with_apt:
+                installer = SystemPackageTool()
+                if self.settings.arch == "x86" and tools.detected_architecture() == "x86_64":
+                    arch_suffix = ':i386'
+                    installer.install("g++-multilib")
+                    installer.install("gcc-multilib")
+                else:
+                    arch_suffix = ''
+                installer.install("pkg-config")
+                installer.install("ninja-build")
+                installer.install("doxygen")
+                installer.install("clang")
+                installer.install("python3")
+                installer.install("git-lfs")
+                installer.install("nasm")
+                installer.install("%s%s" % ("mono-devel"))
+    
+                installer.install("%s%s" % ("libusb-1.0-0-dev", arch_suffix))
+                installer.install("%s%s" % ("libgl1-mesa-dev", arch_suffix))
+                installer.install("%s%s" % ("libsoundio-dev", arch_suffix))
+                installer.install("%s%s" % ("libvulkan-dev", arch_suffix))
+                installer.install("%s%s" % ("libx11-dev", arch_suffix))
+                installer.install("%s%s" % ("libxcursor-dev", arch_suffix))
+                installer.install("%s%s" % ("libxinerama-dev", arch_suffix))
+                installer.install("%s%s" % ("libxrandr-dev", arch_suffix))
+                installer.install("%s%s" % ("libusb-1.0-0-dev", arch_suffix))
+                installer.install("%s%s" % ("libssl-dev", arch_suffix))
+                installer.install("%s%s" % ("libudev-dev", arch_suffix))
+                installer.install("%s%s" % ("mesa-common-dev", arch_suffix))
+                installer.install("%s%s" % ("uuid-dev", arch_suffix))
+            else:
+                self.output.warn("Could not determine package manager, skipping Linux system requirements installation.")
+
+
     def requirements(self):
         userChannel = "ubitrack/stable"
         if self.options.workspaceBuild:
